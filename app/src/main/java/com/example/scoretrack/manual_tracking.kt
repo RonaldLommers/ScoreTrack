6c72f13c2d472eaa3afbbface6845b1ac346426f
aron van veldhuizen <aronvveldhuizen@gmail.com>
2023-12-14 12:04:24 +0100

    s

---
+++ app/src/main/java/com/example/scoretrack/manual_tracking.kt
@@ -0,0 +1,11 @@
+package com.example.scoretrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class manual_tracking : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manual_tracking)
+    }
+}
