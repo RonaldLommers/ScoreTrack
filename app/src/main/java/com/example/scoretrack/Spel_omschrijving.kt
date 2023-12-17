2b2bdb7887bd0d724cd1b0b8bda66ad97cb4be7c
Wei-Lin Kolff <94454879+WeiLinKolff@users.noreply.github.com>
2023-12-17 22:13:25 +0100

    Design spelomschrijving

---
+++ app/src/main/java/com/example/scoretrack/Spel_omschrijving.kt
@@ -0,0 +1,11 @@
+package com.example.scoretrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Spel_omschrijving : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`activity_spel_omschrijving_poker.xml`)
+    }
+}
