1096744d4ec39a1bddefcf761890445e36199b7e
RonaldLommers <112701502+RLommers@users.noreply.github.com>
2023-12-31 17:08:30 +0100

    Main Activity complete

--- app/src/main/java/com/example/scoretrack/MainActivity.kt
+++ app/src/main/java/com/example/scoretrack/MainActivity.kt
@@ -6,11 +6,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val playImageButton = findViewById<ImageButton>(R.id.playImageButton)
+        playImageButton.setOnClickListener {
+            val intent = Intent(this, Play::class.java)
+            startActivity(intent)
+        }
+
+        val savesImageButton = findViewById<ImageButton>(R.id.savesImageButton)
+        savesImageButton.setOnClickListener {
+            val intent = Intent(this, Saves::class.java)
+            startActivity(intent)
+        }
+
+        val settingsImageButton = findViewById<ImageButton>(R.id.settingsImageButton)
+        settingsImageButton.setOnClickListener {
+            val intent = Intent(this, Accountinstellingen::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/scoretrack/Saves.kt
@@ -0,0 +1,11 @@
+package com.example.scoretrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Saves : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saves)
+    }
+}
