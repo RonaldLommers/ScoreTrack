cd03fedb31deb1ac6eb0607cf843b4f0e8f770d5
RonaldLommers <112701502+RLommers@users.noreply.github.com>
2024-01-10 17:51:21 +0100

    saves function in progres

---
+++ app/src/main/java/com/example/scoretrack/BlackjackGame.kt
@@ -0,0 +1,14 @@
+package com.example.scoretrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.scoretrack.PlayerData
+import com.example.scoretrack.GameData
+
+
+class BlackjackGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blackjack_game)
+    }
+}
