f440262e1f756b4a43601b5e854723b55f9bf61d
RonaldLommers <112701502+RLommers@users.noreply.github.com>
2024-01-11 21:54:50 +0100

    update blackjack game

--- app/src/main/java/com/example/scoretrack/PlayerData.kt
+++ app/src/main/java/com/example/scoretrack/PlayerData.kt
@@ -1,3 +1,8 @@
 package com.example.scoretrack
 
-data class PlayerData(val name: String, val stake: Int)
+data class PlayerData(
+    val playerNumber: String,
+    val name: String,
+    var currentStake: Int,
+    var currentBet: Int = 0
+)
