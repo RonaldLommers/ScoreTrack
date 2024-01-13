fcf6f68e775a06477ee44fcfdab04d3d57b80816
Wei-Lin Kolff <94454879+WeiLinKolff@users.noreply.github.com>
2024-01-13 01:40:52 +0100

    coming soon

--- app/src/main/java/com/example/scoretrack/activity_spelomschrijving_comingsoon.kt
+++ app/src/main/java/com/example/scoretrack/activity_spelomschrijving_comingsoon.kt
@@ -1,13 +1,14 @@
 package com.example.scoretrack
 
-import android.content.Intent
-class activity_spelomschrijving_blackjack : AppCompatActivity() {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_spelomschrijving_comingsoon : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_spelomschrijving_blackjack)
+        setContentView(R.layout.activity_spelomschrijving_comingsoon)
     }
 }
-
 
 
 
@@ -16,7 +17,6 @@ class activity_spelomschrijving_blackjack : AppCompatActivity() {
 val button = findViewById<ImageButton>(R.id.bj_terug)
 
 button.setOnClickListener {
-    val intent = Intent(this, activity_spelomschrijving_blackjack::class.java)
+    val intent = Intent(this, activity_spelomschrijving_comingsoon::class.java)
     startActivity(intent)
 }
-
