8f9fb044728a89d32d532480b76a4974b5632c1a
RonaldLommers <112701502+RLommers@users.noreply.github.com>
2024-01-13 19:13:06 +0100

    update

--- app/src/main/java/com/example/scoretrack/BlackjackGame.kt
+++ app/src/main/java/com/example/scoretrack/BlackjackGame.kt
@@ -2,15 +2,13 @@ package com.example.scoretrack
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.gson.Gson
-
 
 class BlackjackGame : AppCompatActivity() {
 
@@ -21,7 +19,9 @@ class BlackjackGame : AppCompatActivity() {
     private lateinit var playerIcons: Array<ImageView?>
     private lateinit var playerNames: Array<TextView?>
     private lateinit var playerStakes: Array<TextView?>
+    private lateinit var playerBets: Array<TextView?>
     private lateinit var showBetView: TextView
+    private lateinit var resetBetButton: Button
     private var players: MutableList<PlayerData> = mutableListOf()
     private var playerCount = 0
     private var currentPlayerIndex = 0
@@ -31,16 +31,39 @@ class BlackjackGame : AppCompatActivity() {
         setContentView(R.layout.activity_blackjack_game)
 
         dealerStakeView = findViewById(R.id.dealerStakeView)
-        showBetView = findViewById(R.id.showBetView) // Assuming this is your TextView ID
+        showBetView = findViewById(R.id.showBetView)
+        resetBetButton = findViewById(R.id.resetBetButton)
         updateDealerStakeUI()
         setupChipClickListeners()
+        setupResetBetButtonListener()
 
+        processIncomingGameData()
+        initializePlayerViews()
+
+        startTurn(currentPlayerIndex)
+    }
+
+    private fun setupResetBetButtonListener() {
+        resetBetButton.setOnClickListener {
+            resetCurrentPlayerBet()
+        }
+    }
+
+    private fun resetCurrentPlayerBet() {
+        if (currentPlayerIndex in players.indices) {
+            val currentPlayer = players[currentPlayerIndex]
+            currentPlayer.currentBet = 0
+            showBetView.text = "Bet: 0"
+            playerBets[currentPlayerIndex]?.text = "Bet: 0"
+            Toast.makeText(this, "Bet reset to 0", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun processIncomingGameData() {
         val gameDataJson = intent.getStringExtra("gameData")
         if (gameDataJson.isNullOrEmpty()) {
             showToastAndNavigateBack()
             return
-        } else {
-            Log.d("BlackjackGame", "Received game data: $gameDataJson")
         }
 
         val gameData = try {
@@ -58,14 +81,32 @@ class BlackjackGame : AppCompatActivity() {
 
         players = gameData.players.toMutableList()
         playerCount = players.size
+    }
 
+    private fun initializePlayerViews() {
+        val constraintLayout = findViewById<ConstraintLayout>(R.id.mainLayout)
         playerIcons = arrayOfNulls(playerCount)
         playerNames = arrayOfNulls(playerCount)
         playerStakes = arrayOfNulls(playerCount)
+        playerBets = arrayOfNulls(playerCount)
 
-        addPlayerViews()
+        addPlayerViews(constraintLayout)
         updatePlayerStakesUI()
-        startTurn(currentPlayerIndex)
+    }
+
+    private fun addPlayerViews(constraintLayout: ConstraintLayout) {
+        for (i in 0 until playerCount) {
+            val player = players[i]
+            val playerIndex = i + 1
+            playerIcons[i] = constraintLayout.findViewById(resources.getIdentifier("playerIcon$playerIndex", "id", packageName))
+            playerNames[i] = constraintLayout.findViewById(resources.getIdentifier("playerName$playerIndex", "id", packageName))
+            playerStakes[i] = constraintLayout.findViewById(resources.getIdentifier("playerStake$playerIndex", "id", packageName))
+            playerBets[i] = constraintLayout.findViewById(resources.getIdentifier("playerBet$playerIndex", "id", packageName))
+
+            playerNames[i]?.text = player.name
+            playerStakes[i]?.text = "Stake: ${player.currentStake}"
+            playerBets[i]?.text = "Bet: 0"
+        }
     }
 
     private fun setupChipClickListeners() {
@@ -81,68 +122,10 @@ class BlackjackGame : AppCompatActivity() {
             val currentPlayer = players[currentPlayerIndex]
             currentPlayer.currentBet += amount
             showBetView.text = "Bet: ${currentPlayer.currentBet}"
-            Toast.makeText(this, "$amount is added", Toast.LENGTH_SHORT).show()
-
-            // Adding a log message to logcat
-            Log.d("BlackjackGame", "Added $amount to current bet. Total bet: ${currentPlayer.currentBet}")
+            playerBets[currentPlayerIndex]?.text = "Bet: ${currentPlayer.currentBet}"
+            Toast.makeText(this, "$amount added", Toast.LENGTH_SHORT).show()
         } else {
             Toast.makeText(this, "Invalid player turn", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-
-    private fun addPlayerViews() {
-        val constraintLayout = findViewById<ConstraintLayout>(R.id.mainLayout)
-        val maxPlayerSlots = 6
-
-        for (i in 1..maxPlayerSlots) {
-            constraintLayout.findViewById<ImageView>(resources.getIdentifier("playerIcon$i", "id", packageName))?.visibility = View.GONE
-            constraintLayout.findViewById<TextView>(resources.getIdentifier("playerName$i", "id", packageName))?.visibility = View.GONE
-            constraintLayout.findViewById<TextView>(resources.getIdentifier("playerStake$i", "id", packageName))?.visibility = View.GONE
-        }
-
-        for (i in 0 until playerCount) {
-            val player = players[i]
-            val playerIconId = resources.getIdentifier("playerIcon${i + 1}", "id", packageName)
-            var playerIcon = constraintLayout.findViewById<ImageView>(playerIconId)
-            if (playerIcon == null) {
-                playerIcon = ImageView(this).apply {
-                    id = View.generateViewId()
-                    setImageResource(R.drawable.ic_player_icon)
-                }
-                constraintLayout.addView(playerIcon)
-            } else {
-                playerIcon.visibility = View.VISIBLE
-            }
-            playerIcons[i] = playerIcon
-
-            val playerNameId = resources.getIdentifier("playerName${i + 1}", "id", packageName)
-            var playerName = constraintLayout.findViewById<TextView>(playerNameId)
-            if (playerName == null) {
-                playerName = TextView(this).apply {
-                    id = View.generateViewId()
-                    text = player.name
-                }
-                constraintLayout.addView(playerName)
-            } else {
-                playerName.text = player.name
-                playerName.visibility = View.VISIBLE
-            }
-            playerNames[i] = playerName
-
-            val playerStakeId = resources.getIdentifier("playerStake${i + 1}", "id", packageName)
-            var playerStake = constraintLayout.findViewById<TextView>(playerStakeId)
-            if (playerStake == null) {
-                playerStake = TextView(this).apply {
-                    id = View.generateViewId()
-                    text = "Stake: ${player.currentStake}"
-                }
-                constraintLayout.addView(playerStake)
-            } else {
-                playerStake.text = "Stake: ${player.currentStake}"
-                playerStake.visibility = View.VISIBLE
-            }
-            playerStakes[i] = playerStake
         }
     }
 
@@ -154,17 +137,14 @@ class BlackjackGame : AppCompatActivity() {
 
     private fun startTurn(playerIndex: Int) {
         highlightCurrentPlayerIcon(playerIndex)
-        // Handle player's turn logic here
     }
 
     private fun highlightCurrentPlayerIcon(playerIndex: Int) {
         for (i in players.indices) {
             val playerIcon = playerIcons[i]
             if (i == playerIndex) {
-                // Set to active icon
                 playerIcon?.setImageResource(R.drawable.ic_player_icon_active)
             } else {
-                // Set to regular icon
                 playerIcon?.setImageResource(R.drawable.ic_player_icon)
             }
         }
