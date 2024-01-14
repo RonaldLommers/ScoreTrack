91f7d34582afdb03f34e012df741a8389b8dd98b
Wei-Lin Kolff <94454879+WeiLinKolff@users.noreply.github.com>
2024-01-14 23:23:35 +0100

    spel omschrijving en regels

---
+++ app/src/main/java/com/example/scoretrack/activity_spelregels_yahtzee.kt
@@ -0,0 +1,11 @@
+package com.example.scoretrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_spelregels_yahtzee : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spelregels_yahtzee)
+    }
+}
